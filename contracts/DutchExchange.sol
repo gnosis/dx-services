846cb2707ec4449f26ba1ef77245ca1a36708e82
Angel Rodriguez <anxolin@gmail.com>
2018-01-24 15:47:55 +0100

    Deploy dutch exchange smart contracts

--- contracts/DutchExchange.sol
+++ contracts/DutchExchange.sol
@@ -1,3 +1,9 @@
 pragma solidity ^0.4.18;
 
+// Main smart contract
 import "@gnosis.pm/dutch-exchange/contracts/DutchExchange.sol";
+
+// Tokens
+import "@gnosis.pm/dutch-exchange/contracts/Tokens/EtherToken.sol";
+import "@gnosis.pm/dutch-exchange/contracts/Tokens/TokenGNO.sol";
+import "@gnosis.pm/dutch-exchange/contracts/Tokens/TokenTUL.sol";
