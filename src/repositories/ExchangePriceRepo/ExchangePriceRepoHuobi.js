7270363de08a508ad4984703ee012b01787c8b57
Daniel Sánchez <daniel.sanchez@gnosis.pm>
2018-01-29 18:03:49 +0100

    Add .eslintrc.js to share lint config
    Configure linter for jest globals
    Fix some issues in repos to test linter

--- src/repositories/ExchangePriceRepo/ExchangePriceRepoHuobi.js
+++ src/repositories/ExchangePriceRepo/ExchangePriceRepoHuobi.js
@@ -1,26 +1,19 @@
 const debug = require('debug')('dx-service:repositories:ExchangePriceRepoHuobi')
 const httpRequest = require('../../helpers/httpRequest')
 
-
-// Default options
-const defaults = {
-	url: 'https://api.huobi.pro',
-	version: 'v1',
-	timeout: 5000,
-}
-
 class ExchangePriceRepoHuobi {
-  constructor (options) {
-    this._config = Object.assign(defaults, options)
+  constructor ({ url = 'https://api.huobi.pro', version = 'v1', timeout = 5000 }) {
+    this._timeout = timeout
+    this._baseUrl = url
   }
 
   async getPrice ({tokenA, tokenB}) {
     debug('Get price for %s-%s', tokenA, tokenB)
 
-    const url = this._config.url + '/market/detail/merged?symbol=' + tokenA.toLowerCase() + tokenB.toLowerCase()
+    const url = this._baseUrl + '/market/detail/merged?symbol=' + tokenA.toLowerCase() + tokenB.toLowerCase()
     debug('Huobi request url: ', url)
 
-		const request = {url, method: 'GET', data: {}, timeout: this._config.timeout }
+    const request = { url, method: 'GET', data: {}, timeout: this._timeout }
     const response = await httpRequest.rawRequest(request, {})
     debug('Huobi Response to ' + tokenA + tokenB + ': ', response.tick.close.toString())
     return response.tick.close.toString()
--- tests/services/AuctionService.test.js
+++ tests/services/AuctionService.test.js
@@ -2,8 +2,8 @@ const {
   auctionService
 } = require('../../src/helpers/instanceFactory')
 
-test ('ensureSellLiquidity throws not implemented', () => {
-  expect (() => { // Wrap the code to catch the exception
+test('ensureSellLiquidity throws not implemented', () => {
+  expect(() => { // Wrap the code to catch the exception
     auctionService.ensureSellLiquidity({
       tokenA: 'RDN',
       tokenB: 'ETH'
