3f5963c644ee3aa818ca3fb87f4aa707b5eedcd7
Angel Rodriguez <anxolin@gmail.com>
2018-01-29 13:23:30 +0100

    Add test networks and live network to truffle

--- truffle.js
+++ truffle.js
@@ -1,5 +1,3 @@
-// const = require('conf/config')
-
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   networks: {
@@ -8,8 +6,35 @@ module.exports = {
       port: 8545,
       network_id: '*', // Match any network id
       gas: 6700000
+    },
+
+    // Kovan
+    //  truffle migrate --network kovan
+    //  parity --mode active --chain kovan --port 8546
+    kovan: {
+      host: '127.0.0.1',
+      port: 8546,
+      network_id: 42,
+      gas: 6700000
+    },
+
+    // Ronkeby
+    //  truffle migrate --network kovan
+    rinkeby: {
+      host: '127.0.0.1',
+      port: 8547, // parity --mode active --chain rinkeby --port 8548
+      network_id: 4,
+      gas: 6700000
+    },
+
+    live: {
+      host: '127.0.0.1',
+      port: 8546, // parity --mode active --chain rinkeby --port 8548
+      network_id: 1,
+      gas: 6700000
     }
   },
+
   solc: {
     optimizer: {
       enabled: true,
