863d0ba1969407da955adcb3dfc11d9b4529c62f
Daniel Sánchez <daniel.sanchez@gnosis.pm>
2018-02-23 17:22:02 +0100

    AuctionRepo test cleanup

--- tests/repositories/AuctionRepo.test.js
+++ tests/repositories/AuctionRepo.test.js
@@ -13,13 +13,13 @@ beforeEach(async () => {
 
 afterEach(async () => {
   const { ethereumClient } = await setupPromise
-  // console.log('Current snapshotId to revert is : ' + currentSnapshotId)
 
   return ethereumClient.revertSnapshot(currentSnapshotId)
 })
 
+// Test to approve one token
 test('Approve token', async () => {
-  const {auctionRepo, owner} = await setupPromise
+  const { auctionRepo, owner } = await setupPromise
 
   expect(await auctionRepo.isApprovedToken({ token: 'RDN' })).toBeFalsy()
 
@@ -28,67 +28,47 @@ test('Approve token', async () => {
   expect(await auctionRepo.isApprovedToken({ token: 'RDN' })).toBeTruthy()
 })
 
+// Add a new token pair
 test('Add token pair', async () => {
-  const {web3, auctionRepo, user1, buySell, setupTestCases} = await setupPromise
+  const { auctionRepo, setupTestCases } = await setupPromise
 
   await setupTestCases()
 
-  expect(await auctionRepo.isApprovedMarket({ tokenA: 'RDN', tokenB: 'ETH' })).toBeFalsy()
+  expect(await auctionRepo.isApprovedMarket({ tokenA: 'RDN', tokenB: 'ETH' }))
+    .toBeFalsy()
 
-  expect(await auctionRepo.getStateInfo({sellToken: 'RDN', buyToken: 'ETH'})).toEqual(
-    {'auction': null, 'auctionIndex': 0, 'auctionOpp': null, 'auctionStart': null}
-  )
-  expect(await auctionRepo.getState({sellToken: 'RDN', buyToken: 'ETH'})).toEqual(
-    'UNKNOWN_TOKEN_PAIR'
-  )
+  expect(await auctionRepo.getStateInfo({sellToken: 'RDN', buyToken: 'ETH'}))
+    .toEqual({'auction': null, 'auctionIndex': 0, 'auctionOpp': null, 'auctionStart': null})
 
-  const result = await auctionRepo.addTokenPair({
-    from: user1,
-    tokenA: 'RDN',
-    tokenAFunding: web3.toWei(0, 'ether'),
-    tokenB: 'ETH',
-    tokenBFunding: web3.toWei(13.123, 'ether'),
-    initialClosingPrice: {
-      numerator: 4079,
-      denominator: 1000000
-    }
-  })
+  expect(await auctionRepo.getState({sellToken: 'RDN', buyToken: 'ETH'}))
+    .toEqual('UNKNOWN_TOKEN_PAIR')
 
-  expect(await auctionRepo.getStateInfo({sellToken: 'RDN', buyToken: 'ETH'})).toMatchObject({
-    'auctionIndex': 1,
-    'auction': {'buyVolume': new BigNumber(0), 'closingPrice': null, 'isClosed': false, 'isTheoreticalClosed': false, 'sellVolume': new BigNumber(0)},
-    'auctionOpp': {'buyVolume': new BigNumber(0), 'closingPrice': null, 'isClosed': false, 'isTheoreticalClosed': false, 'sellVolume': new BigNumber('13062839545454545454')}
-  })
-  expect(await auctionRepo.getState({sellToken: 'RDN', buyToken: 'ETH'})).toEqual(
-    'WAITING_FOR_AUCTION_TO_START'
-  )
+  await _addRdnEthTokenPair()
 
-  expect(await auctionRepo.isApprovedMarket({tokenA: 'RDN', tokenB: 'ETH'})).toBeTruthy()
+  expect(await auctionRepo.getStateInfo({sellToken: 'RDN', buyToken: 'ETH'}))
+    .toMatchObject(baseMarketState)
+
+  expect(await auctionRepo.getState({sellToken: 'RDN', buyToken: 'ETH'}))
+    .toEqual('WAITING_FOR_AUCTION_TO_START')
+
+  expect(await auctionRepo.isApprovedMarket({tokenA: 'RDN', tokenB: 'ETH'}))
+    .toBeTruthy()
 })
 
-test('Add sell to auction', async () => {
+// Add funds to auction
+test('Add funds to auction', async () => {
   jest.setTimeout(10000)
-  const {web3, auctionRepo, user1, buySell, setupTestCases} = await setupPromise
+  const { auctionRepo, user1, buySell, setupTestCases } = await setupPromise
 
   await setupTestCases()
 
-  expect(await auctionRepo.isApprovedMarket({ tokenA: 'RDN', tokenB: 'ETH' })).toBeFalsy()
+  expect(await auctionRepo.isApprovedMarket({ tokenA: 'RDN', tokenB: 'ETH' }))
+    .toBeFalsy()
 
-  expect(await auctionRepo.getState({sellToken: 'RDN', buyToken: 'ETH'})).toEqual(
-    'UNKNOWN_TOKEN_PAIR'
-  )
+  expect(await auctionRepo.getState({sellToken: 'RDN', buyToken: 'ETH'}))
+    .toEqual('UNKNOWN_TOKEN_PAIR')
 
-  const result = await auctionRepo.addTokenPair({
-    from: user1,
-    tokenA: 'RDN',
-    tokenAFunding: web3.toWei(0, 'ether'),
-    tokenB: 'ETH',
-    tokenBFunding: web3.toWei(13.123, 'ether'),
-    initialClosingPrice: {
-      numerator: 4079,
-      denominator: 1000000
-    }
-  })
+  await _addRdnEthTokenPair()
 
   await buySell('postSellOrder', {
     from: user1,
@@ -97,31 +77,84 @@ test('Add sell to auction', async () => {
     amount: parseFloat('2')
   })
 
-  expect(await auctionRepo.getStateInfo({sellToken: 'RDN', buyToken: 'ETH'})).toMatchObject({
-    'auctionIndex': 1,
-    'auction': {'buyVolume': new BigNumber('0'), 'closingPrice': null, 'isClosed': false, 'isTheoreticalClosed': false, 'sellVolume': new BigNumber('1990000000000000000')},
-    'auctionOpp': {'buyVolume': new BigNumber('0'), 'closingPrice': null, 'isClosed': false, 'isTheoreticalClosed': false, 'sellVolume': new BigNumber('13062839545454545454')}
-  })
-  expect(await auctionRepo.getState({sellToken: 'RDN', buyToken: 'ETH'})).toEqual(
-    'WAITING_FOR_AUCTION_TO_START'
-  )
+  expect(await auctionRepo.getStateInfo({sellToken: 'RDN', buyToken: 'ETH'}))
+    .toMatchObject(Object.assign(
+      {}, baseMarketState, { auction: { sellVolume: new BigNumber('1990000000000000000') } })
+    )
+  expect(await auctionRepo.getState({sellToken: 'RDN', buyToken: 'ETH'}))
+    .toEqual('WAITING_FOR_AUCTION_TO_START')
 
-  expect(await auctionRepo.isApprovedMarket({tokenA: 'RDN', tokenB: 'ETH'})).toBeTruthy()
+  expect(await auctionRepo.isApprovedMarket({tokenA: 'RDN', tokenB: 'ETH'}))
+    .toBeTruthy()
 })
 
+// Test buy tokens in auction
 test('Buy in auction', async () => {
   jest.setTimeout(10000)
-  const {web3, auctionRepo, user1, buySell, setupTestCases, ethereumClient} = await setupPromise
+  const { auctionRepo, user1, buySell, setupTestCases, ethereumClient } = await setupPromise
 
   await setupTestCases()
 
-  expect(await auctionRepo.isApprovedMarket({ tokenA: 'RDN', tokenB: 'ETH' })).toBeFalsy()
+  expect(await auctionRepo.isApprovedMarket({ tokenA: 'RDN', tokenB: 'ETH' }))
+    .toBeFalsy()
 
-  expect(await auctionRepo.getState({sellToken: 'RDN', buyToken: 'ETH'})).toEqual(
-    'UNKNOWN_TOKEN_PAIR'
-  )
+  expect(await auctionRepo.getState({sellToken: 'RDN', buyToken: 'ETH'}))
+    .toEqual('UNKNOWN_TOKEN_PAIR')
 
-  const result = await auctionRepo.addTokenPair({
+  await _addRdnEthTokenPair()
+
+  expect(await auctionRepo.getState({sellToken: 'RDN', buyToken: 'ETH'}))
+    .toEqual('WAITING_FOR_AUCTION_TO_START')
+
+  expect(await auctionRepo.getState({sellToken: 'ETH', buyToken: 'RDN'}))
+    .toEqual('WAITING_FOR_AUCTION_TO_START')
+
+  // console.log(await ethereumClient.geLastBlockTime())
+  await ethereumClient.increaseTime(6.1 * 60 * 60)
+  // console.log(await ethereumClient.geLastBlockTime())
+
+  expect(await auctionRepo.getState({sellToken: 'RDN', buyToken: 'ETH'}))
+    .toEqual('RUNNING')
+
+  expect(await auctionRepo.getState({sellToken: 'ETH', buyToken: 'RDN'}))
+    .toEqual('RUNNING')
+
+  await buySell('postBuyOrder', {
+    from: user1,
+    sellToken: 'ETH',
+    buyToken: 'RDN',
+    amount: parseFloat('0.5')
+  })
+
+  expect(await auctionRepo.getStateInfo({sellToken: 'RDN', buyToken: 'ETH'}))
+    .toMatchObject(Object.assign(
+      {}, baseMarketState, {auctionOpp: {buyVolume: new BigNumber('497500000000000000')}})
+    )
+})
+
+// ********* Test helpers *********
+const baseMarketState = {
+  'auctionIndex': 1,
+  'auction': {
+    'buyVolume': new BigNumber(0),
+    'closingPrice': null,
+    'isClosed': false,
+    'isTheoreticalClosed': false,
+    'sellVolume': new BigNumber(0)
+  },
+  'auctionOpp': {
+    'buyVolume': new BigNumber(0),
+    'closingPrice': null,
+    'isClosed': false,
+    'isTheoreticalClosed': false,
+    'sellVolume': new BigNumber('13062839545454545454')
+  }
+}
+
+async function _addRdnEthTokenPair () {
+  const { web3, auctionRepo, user1 } = await setupPromise
+
+  await auctionRepo.addTokenPair({
     from: user1,
     tokenA: 'RDN',
     tokenAFunding: web3.toWei(0, 'ether'),
@@ -132,35 +165,4 @@ test('Buy in auction', async () => {
       denominator: 1000000
     }
   })
-
-  expect(await auctionRepo.getState({sellToken: 'RDN', buyToken: 'ETH'})).toEqual(
-    'WAITING_FOR_AUCTION_TO_START'
-  )
-  expect(await auctionRepo.getState({sellToken: 'ETH', buyToken: 'RDN'})).toEqual(
-    'WAITING_FOR_AUCTION_TO_START'
-  )
-
-  // console.log(await ethereumClient.geLastBlockTime())
-  await ethereumClient.increaseTime(6.1 * 60 * 60)
-  // console.log(await ethereumClient.geLastBlockTime())
-
-  expect(await auctionRepo.getState({sellToken: 'RDN', buyToken: 'ETH'})).toEqual(
-    'RUNNING'
-  )
-  expect(await auctionRepo.getState({sellToken: 'ETH', buyToken: 'RDN'})).toEqual(
-    'RUNNING'
-  )
-
-  await buySell('postBuyOrder', {
-    from: user1,
-    sellToken: 'ETH',
-    buyToken: 'RDN',
-    amount: parseFloat('0.5')
-  })
-
-  expect(await auctionRepo.getStateInfo({sellToken: 'RDN', buyToken: 'ETH'})).toMatchObject({
-    'auctionIndex': 1,
-    'auction': {'buyVolume': new BigNumber('0'), 'closingPrice': null, 'isClosed': false, 'isTheoreticalClosed': false, 'sellVolume': new BigNumber('0')},
-    'auctionOpp': {'buyVolume': new BigNumber('497500000000000000'), 'closingPrice': null, 'isClosed': false, 'isTheoreticalClosed': false, 'sellVolume': new BigNumber('13062839545454545454')}
-  })
-})
+}
