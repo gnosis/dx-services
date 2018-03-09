6b111bfa1c24fbf66194700850558890154f2266
Angel Rodriguez <anxolin@gmail.com>
2018-03-09 12:18:04 +0100

    Handle prize 0 for status on bot-cli

--- tests/helpers/testSetup.js
+++ tests/helpers/testSetup.js
@@ -381,27 +381,33 @@ async function getHelpers ({ ethereumClient, auctionRepo, ethereumRepo, config }
         auctionIndex: auctionIndex - 1
       })
 
-      const priceRelationshipPercentage = price.numerator
-        .mul(closingPrice.denominator)
-        .div(price.denominator)
-        .div(closingPrice.numerator)
-        .mul(100)
-        .toFixed(2)
-
+      let buyVolumesInSellTokens, priceRelationshipPercentage
+      if (price.numerator.isZero()) {
+        // The auction runned for too long
+        buyVolumesInSellTokens = auction.sellVolume
+        priceRelationshipPercentage = 'N/A'
+      } else {
+        // Get the number of sell tokens that we can get for the buyVolume
+        buyVolumesInSellTokens = price.denominator.times(auction.buyVolume).div(price.numerator)
+        priceRelationshipPercentage = price.numerator
+          .mul(closingPrice.denominator)
+          .div(price.denominator)
+          .div(closingPrice.numerator)
+          .mul(100)
+          .toFixed(2) + ' %'
+      }
+      const boughtPercentage = 100 - 100 * (auction.sellVolume - buyVolumesInSellTokens) / auction.sellVolume
+      // debug(`\t\tBuy volume (in sell tokens):`, formatFromWei(buyVolumesInSellTokens.toNumber()))
+      
       debug(`\t\tPrice:`)
       debug(`\t\t\tPrevious Closing Price: %s %s/%s`, fractionFormatter(closingPrice),
         tokenB, tokenA)
       debug(`\t\t\tCurrent Price: %s %s/%s`, fractionFormatter(price),
         tokenB, tokenA)
-      debug(`\t\t\tPrice relation: %d %`, priceRelationshipPercentage)
-
-      const buyVolumesInSellTokens = price.denominator.times(auction.buyVolume).div(price.numerator)
-      const boughtPercentage = 100 - 100 * (auction.sellVolume - buyVolumesInSellTokens) / auction.sellVolume
-      // debug(`\t\tBuy volume (in sell tokens):`, formatFromWei(buyVolumesInSellTokens.toNumber()))
-
+      debug(`\t\t\tPrice relation: %s`, priceRelationshipPercentage)
       debug('\t\tBuy volume:')
       debug(`\t\t\tbuyVolume: %d %s`, formatFromWei(auction.buyVolume), tokenB)
-      debug(`\t\t\tBought percentage: %d %`, boughtPercentage.toFixed(4))
+      debug(`\t\t\tBought percentage: %s %`, boughtPercentage.toFixed(4))
       if (state.indexOf('WAITING') === -1) {
         // Show outstanding volumen if we are not in a waiting period
         const outstandingVolume = await auctionRepo.getOutstandingVolume({
