0c6f7007a720a871c6460cf7725544ff5c2f5bd0
Angel Rodriguez <anxolin@gmail.com>
2018-03-14 11:53:33 +0100

    Improve error handling

--- src/helpers/gracefullShutdown.js
+++ src/helpers/gracefullShutdown.js
@@ -3,26 +3,32 @@ const Logger = require('../helpers/Logger')
 const logger = new Logger(loggerNamespace)
 const POSIX_SIGNALS = ['SIGINT', 'SIGTERM', 'SIGQUIT']
 const listerners = []
+let shuttedDown = false
 
 POSIX_SIGNALS.forEach(signal => {
   process.on(signal, () => {
-    function exit (returnCode) {
-      logger.info('The app is ready to shutdown! Good bye! :)')
-      process.exit(returnCode)
-    }
-
-    shutDown(signal)
-      .then(() => {
-        exit(0)
-      })
-      .catch(error => {
-        logger.error({
-          msg: 'Error shuttting down the app: ' + error.toString(),
-          error
-        })
-        exit(2)
-      })
+    _doShutDown(`I've gotten a ${signal} signal`)
   })
+})
+
+process.on('uncaughtException', error => {
+  logger.error({
+    msg: 'Uncought exception: ' + error.toString(),
+    error
+  })
+
+  // TODO: Decide if we want to shutdown the app here or not
+  // _doShutDown(`There was a glonal unhandled exception`)
+})
+
+process.on('unhandledRejection', error => {
+  logger.error({
+    msg: 'Uncought promise rejection: ' + error.toString(),
+    error
+  })
+
+  // TODO: Decide if we want to shutdown the app here or not
+  // _doShutDown(`There was a glonal uncought global exception`)
 })
 
 function onShutdown (listener) {
@@ -30,17 +36,38 @@ function onShutdown (listener) {
   listerners.push(listener)
 }
 
-async function shutDown (signal) {
-  if (signal) {
-    logger.info("I've gotten a %o signal! Closing gracefully...", signal)
+async function shutDown (reason) {
+  if (!shuttedDown) {
+    shuttedDown = true
+    let reasonPrefix = reason ? reason + ': ' : ''
+    logger.info(reasonPrefix + 'Closing gracefully...')
+
+    // Wait for all shutdow listeners
+    await Promise.all(
+      listerners.map(listener => {
+        return listener()
+      })
+    )
+  }
+}
+
+function _doShutDown (reason) {
+  function _doExit (returnCode) {
+    logger.info('The app is ready to shutdown! Good bye! :)')
+    process.exit(returnCode)
   }
 
-  // Wait for all shutdow listeners
-  await Promise.all(
-    listerners.map(listener => {
-      return listener()
+  shutDown(reason)
+    .then(() => {
+      _doExit(0)
     })
-  )
+    .catch(error => {
+      logger.error({
+        msg: 'Error shuttting down the app: ' + error.toString(),
+        error
+      })
+      _doExit(2)
+    })
 }
 
 module.exports = {
