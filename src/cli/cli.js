56633b39807846a9526a1c447b47ad0fa9175b52
Angel Rodriguez <anxolin@gmail.com>
2018-03-20 17:32:34 +0100

    Add buy liquidity command

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -15,11 +15,18 @@ async function run (instances) {
   const cli = yargs.usage('$0 <cmd> [args]')
   const commandParams = { cli, instances, logger }
 
-  // Commands
-  // require('./commands/hiCmd')(cli)
-  // require('./commands/helloCmd')(cli)
+  // Info commands
   require('./commands/printStateCmd')(commandParams)
-  
+
+  // Trade commands
+  // buy, sell, ..
+
+  // Liquidity commands
+  require('./commands/buyLiquidityCmd')(commandParams)
+
+  // Setup commands (we might need to move this ones to `setup` cli)
+  // add-token-pair, add-funding-for-test-user,...
+
   var argv = cli
     .help()
     .strict()
---
+++ src/cli/commands/buyLiquidityCmd.js
@@ -0,0 +1,52 @@
+const printState = require('../helpers/printState')
+
+function registerCommand ({ cli, instances, logger }) {
+  cli.command(
+    'buy-liquidity <sellToken> <buyToken>',
+    'Ensure the buy liquidity for a token pair',
+    yargs => {
+      yargs.positional('sellToken', {
+        type: 'string',
+        default: 'ETH',
+        describe: 'Name of the sell token'
+      })
+      yargs.positional('buyToken', {
+        type: 'string',
+        default: 'RDN',
+        describe: 'Name of the buy token'
+      })
+    }, async function (argv) {
+      const { sellToken, buyToken } = argv
+      const {
+        botAccount,
+        liquidityService
+      } = instances
+      logger.info(`Ensure the BUY liquidity for ${sellToken}-${buyToken}`)
+      const soldTokens = await liquidityService.ensureBuyLiquidity({
+        sellToken,
+        buyToken,
+        from: botAccount
+      })
+
+      if (soldTokens) {
+        // The bot sold some tokens
+        logger.info({
+          sellToken,
+          buyToken,
+          msg: "I've bought %d %s (%d USD) to ensure liquidity",
+          params: [
+            soldTokens.amount.div(1e18),
+            soldTokens.sellToken,
+            soldTokens.amountInUSD
+          ]
+        })
+      } else {
+        // The bot didn't have to do anything
+        logger.info({
+          msg: 'There\'s no need to ensure buy liquidity'
+        })
+      }
+    })
+}
+
+module.exports = registerCommand
