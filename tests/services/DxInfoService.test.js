ff9bbf2cf141fef6b79810b82fa880d849a8e5c9
Daniel Sánchez <daniel.sanchez@gnosis.pm>
2018-03-21 17:57:13 +0100

    Fix getAuctionIndex test
    Add getCurrent price for auction test

--- tests/services/DxInfoService.test.js
+++ tests/services/DxInfoService.test.js
@@ -4,9 +4,11 @@ const auctionRepoMock = new AuctionRepoMock({})
 
 const auctionsMockData = require('../data/auctions')
 
+const BigNumber = require('bignumber.js')
+
 const setupPromise = testSetup()
 
-test.only('It should return available markets', async () => {
+test('It should return available markets', async () => {
   const { dxInfoService } = await setupPromise
 
   dxInfoService._auctionRepo = auctionRepoMock
@@ -17,18 +19,17 @@ test.only('It should return available markets', async () => {
   expect(await dxInfoService.getMarkets()).toEqual(EXPECTED_MARKETS)
 })
 
-test.only('It should return auction index', async () => {
+test('It should return auction index', async () => {
   const { dxInfoService } = await setupPromise
 
   dxInfoService._auctionRepo = auctionRepoMock
-  const EXPECTED_MARKETS = [
-    {tokenA: 'ETH', tokenB: 'RDN'}, {tokenA: 'ETH', tokenB: 'OMG'}
-  ]
 
-  expect(await dxInfoService.getMarkets()).toEqual(EXPECTED_MARKETS)
+  let rdnEthAuctionIndex = await dxInfoService.getAuctionIndex({
+    sellToken: 'RDN', buyToken: 'ETH' })
+  expect(rdnEthAuctionIndex).toBe(77)
 })
 
-test('It should return market details', async () => {
+test.skip('It should return market details', async () => {
   const { dxInfoService } = await setupPromise
 
   dxInfoService._auctionRepo = auctionRepoMock
@@ -36,7 +37,7 @@ test('It should return market details', async () => {
   expect(await dxInfoService.getMarketDetails({ sellToken: 'RDN', buyToken: 'ETH' })).toBe('')
 })
 
-test('It should return auction details', async () => {
+test.skip('It should return auction details', async () => {
   const { dxInfoService } = await setupPromise
 
   dxInfoService._auctionRepo = auctionRepoMock
@@ -54,11 +55,20 @@ test('It should return auction details', async () => {
   expect(await dxInfoService.getAuctions({currencyA: 'RDN', currencyB: 'ETH'})).toBe(rdnEthAuctions)
 })
 
-// test('Get current auction price for two tokens', async () => {
-// const { dxInfoService } = await setupPromise
-//
-//   expect(await dxInfoService.getCurrentPrice({sellToken: 'RDN', buyToken: 'ETH'})).toBe('')
-// })
+test('It should return current auction price', async () => {
+  const { dxInfoService } = await setupPromise
+
+  dxInfoService._auctionRepo = auctionRepoMock
+
+  const RDN_ETH_CURRENT_PRICE = {
+    denominator: new BigNumber('233'),
+    numerator: new BigNumber('10')
+  }
+
+  let rdnEthCurrentPrice = await dxInfoService.getCurrentPrice({
+    sellToken: 'RDN', buyToken: 'ETH' })
+  expect(rdnEthCurrentPrice).toMatchObject(RDN_ETH_CURRENT_PRICE)
+})
 
 // test('Get balances for all currencies of an account', async () => {
 // const { dxInfoService } = await setupPromise
