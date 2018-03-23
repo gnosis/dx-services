e892cecdecb618b92b2c2ac8120d97ff837c188e
Daniel Sánchez <daniel.sanchez@gnosis.pm>
2018-03-23 16:41:15 +0100

    Refactor HuobiPriceRepo test

--- tests/repositories/ExchangePriceRepo/ExchangePriceRepoHuobi.test.js
+++ tests/repositories/ExchangePriceRepo/ExchangePriceRepoHuobi.test.js
@@ -6,8 +6,17 @@ const exchangePriceRepo = new ExchangePriceRepoHuobi({})
 test('It should return a price for known Crypto markets', async () => {
   jest.setTimeout(10000)
   expect.assertions(2)
-  expect(await exchangePriceRepo.getPrice({tokenA: 'RDN', tokenB: 'ETH'})).toMatch(/\d*\.?\d+/)
-  expect(await exchangePriceRepo.getPrice({tokenA: 'OMG', tokenB: 'ETH'})).toMatch(/\d*\.?\d+/)
+  // WHEN we query for RDN-ETH pair (being ETH tokenB)
+  let rdnEthPrice = await exchangePriceRepo.getPrice({
+    tokenA: 'RDN', tokenB: 'ETH' })
+  // WHEN we query for ETH-RDN asking (being ETH tokenA)
+  let ethRDNPrice = await exchangePriceRepo.getPrice({
+    tokenA: 'ETH', tokenB: 'RDN' })
+
+  // THEN In both cases we get a price number
+  // It's important because in Huobi token order matters
+  expect(rdnEthPrice).toMatch(/\d*\.?\d+/)
+  expect(ethRDNPrice).toMatch(/\d*\.?\d+/)
 })
 
 test('It should throw an error for unknown Crypto markets', async () => {
