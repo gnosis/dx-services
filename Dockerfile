3e7239aa225efbebee4a957c9601b8bf102ce031
Angel Rodriguez <anxolin@gmail.com>
2018-04-18 18:29:09 +0200

    Create cron app taks

--- Dockerfile
+++ Dockerfile
@@ -22,7 +22,7 @@ RUN apk add --update --no-cache --virtual build-dependencies git python make g++
 
 COPY . .
 COPY src/bots/bots-cron-tasks.cfg /etc/crontabs/root
-RUN chmod +x src/bots/run-bots.sh
+RUN chmod +x src/run-bots.sh
 
 # If you are building your code for production
 # RUN npm install --only=production
---
+++ src/tasks/claimFunds.js
@@ -0,0 +1,34 @@
+const loggerNamespace = 'dx-service:tasks:claimFunds'
+const Logger = require('../helpers/Logger')
+const logger = new Logger(loggerNamespace)
+
+// Helpers
+const gracefullShutdown = require('../helpers/gracefullShutdown')
+const instanceFactory = require('../helpers/instanceFactory')
+
+// Env
+const environment = process.env.NODE_ENV
+
+logger.info('Claiming funds for %s', environment)
+
+// Run app
+instanceFactory({})
+  .then(instances => {
+    logger.info('Fake claiming funds...')
+    logger.info('Done')
+  })
+  .catch(error => {
+    // Handle boot errors
+    handleError(error)
+
+    // Shutdown app
+    return gracefullShutdown.shutDown()
+  })
+
+function handleError (error) {
+  process.exitCode = 1
+  logger.error({
+    msg: 'Error booting the application: ' + error.toString(),
+    error
+  })
+}
