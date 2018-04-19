a736bf67ad97743b90ba0a06518614713daa909c
Angel Rodriguez <anxolin@gmail.com>
2018-04-19 13:30:15 +0200

    Add date manipulation utils

--- src/helpers/dateUtil.js
+++ src/helpers/dateUtil.js
@@ -1,1 +1,21 @@
-module.exports = {}
+const moment = require('moment')
+
+function addPeriod (date, amount, period) {
+  return moment(date)
+    .add(amount, period)
+    .toDate()
+}
+
+function toStartOf (date, period) {
+  return moment(date).startOf(period)
+}
+
+function toEndOf (date, period) {
+  return moment(date).endOf(period)
+}
+
+module.exports = {
+  addPeriod,
+  toStartOf,
+  toEndOf
+}
