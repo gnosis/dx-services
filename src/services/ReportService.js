3fcacd045343eb72aca0461cf08e8bee6ec3ddc2
Angel Rodriguez <anxolin@gmail.com>
2018-04-20 11:07:29 +0200

    Send mock file to Slack

--- src/services/ReportService.js
+++ src/services/ReportService.js
@@ -4,6 +4,11 @@ const formatUtil = require('../helpers/formatUtil')
 const logger = new Logger(loggerNamespace)
 const assert = require('assert')
 const fs = require('fs')
+const TEST_FILE = `Auction index;Sell token;Buy token;Sell volume;Buy volume;Last closing price;Price increment;Bot sell volume;Bot buy volume;Ensured sell volume;Ensured buy volume\
+1;ETH;RDN;0,9;0,9;300;N/A;0.9;0.9;100,00%;100,00%
+1;RDN;ETH;0;0;0,003333333;N/A;0;0;0,00%;0,00%
+2;ETH;RDN;1,4;1,4;330;10,00%;0,85;0,95;60,71%;0,63%
+2;RDN;ETH;150;150;0,003030303;-10,00%;120;33;80,00%;22,00%`
 
 let requestId = 1
 
@@ -98,12 +103,15 @@ class ReportService {
       formatUtil.formatDateTime(toDate)
     )
 
+    //  ReadableStream or a Buffer
+    const content = Buffer.from(TEST_FILE, 'utf8')
+
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         resolve({
           name: 'auctions-reports.csv',
           mimeType: 'text/csv',
-          content: null
+          content
         })
       }, 3000)
     })
@@ -114,14 +122,9 @@ class ReportService {
 
     // Upload file to slack
     logger.info('[requestId=%d] Uploading file "%s" to Slack', id, fileName)
-    // const { file: fileSlack } = await this._slackClient.uploadFile({
-    //   fileName,
-    //   file: fileContent,
-    //   channels: channel
-    // })
-    const { file: fileSlack } = await this._slackClient.uploadContentFile({
+    const { file: fileSlack } = await this._slackClient.uploadFile({
       fileName,
-      content: 'hola;que;tal',
+      file: fileContent,
       channels: channel
     })
 
