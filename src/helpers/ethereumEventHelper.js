9c1868e23c66292a911f8ad8194a50efbf1a1f13
Angel Rodriguez <anxolin@gmail.com>
2018-04-23 15:31:15 +0200

    Implement the filter events function

--- src/helpers/ethereumEventHelper.js
+++ src/helpers/ethereumEventHelper.js
@@ -1,25 +1,68 @@
 const debug = require('debug')('DEBUG-dx-service:helpers:ethereumEventHelper')
 const assert = require('assert')
 
-function filter (args) {
-  // TODO: Notify the events in order?
-  return _filterWatchAux(args, 'get')
+/**
+ * Return a promise with the events that match the criteria
+ */
+async function filter ({
+  contract,
+  filters = null,
+  fromBlock = 0,
+  toBlock = 'latest',
+  events = null
+}) {
+  // require events
+  assert(events, '"events" is a required param')
+
+  debug('Filtering contract\'s %s events: %s',
+    contract.address,
+    events ? events.join(', ') : 'all'
+  )
+  const eventsListsPromises = events.map(event => {
+    debug('Get events %s from block %s to %s with filter: %o',
+      event, fromBlock, toBlock, filters)
+    return _getEvents(contract, event, filters, fromBlock, toBlock)
+  })
+
+  return Promise
+    .all(eventsListsPromises)
+    .then(eventsLists => {
+      return eventsLists.reduce((acc, events) => {
+        return acc.concat(events)
+      }, [])
+    })
 }
 
-function watch (args) {
-  return _filterWatchAux(args, 'watch')
+async function _getEvents (contract, event, filters, fromBlock, toBlock) {
+  return new Promise((resolve, reject) => {
+    const eventObject = contract[event](filters, {
+      fromBlock,
+      toBlock
+    })
+    eventObject.get((error, events) => {
+      if (error) {
+        reject(error)
+      } else {
+        // Notify event
+        debug('%d events match the filter', events.length)
+        resolve(events)
+      }
+    })
+  })
 }
 
-function _filterWatchAux ({
+/**
+ *  Listen for events
+ */
+function watch ({
   contract,
   callback,
   filters = null,
   fromBlock = 0,
   toBlock = 'latest',
   events = null
-}, watchMethod) {
-  debug('%s for contract %s events: %s',
-    watchMethod,
+}) {
+  debug('Watching contract\'s %s events: %s',
     contract.address,
     events ? events.join(', ') : 'all'
   )
@@ -30,7 +73,7 @@ function _filterWatchAux ({
     // Subscribe to all events
     assert.equal(filters, null, 'When filtering all the events, the parameter "filter" is allowed')
     const eventObject = contract.allEvents(aditionalFilters)
-    _getAndNotifyEvents(eventObject, callback, watchMethod)
+    _watchAndNotify(eventObject, callback)
 
     // Allow to stop watching
     stopWatching = () => {
@@ -43,7 +86,8 @@ function _filterWatchAux ({
     events.forEach(event => {
       debug('Subscribe to event %s', event)
       const eventObject = contract[event](filters, aditionalFilters)
-      _getAndNotifyEvents(eventObject, callback, watchMethod)
+      stopWatchingFunctions.push(eventObject.stopWatching)
+      _watchAndNotify(eventObject, callback)
     })
 
     // Allow to stop watching
@@ -59,26 +103,14 @@ function _filterWatchAux ({
   }
 }
 
-function _notifyEvent (log, callback) {
-  debug('[%d] New log "%s" - %o', log.logIndex, log.event, log.args)
-  callback(null, log)
-}
-
-function _getAndNotifyEvents (eventObject, callback, watchMethod) {
-  eventObject[watchMethod]((error, log) => {
+function _watchAndNotify (eventObject, callback, isWatch) {
+  eventObject.watch((error, event) => {
     if (error) {
       callback(error)
     } else {
-      // console.log(log)
-      if (Array.isArray(log)) {
-        // Notify events
-        log.map(logAux => {
-          _notifyEvent(logAux, callback)
-        })
-      } else {
-        // Notify event
-        _notifyEvent(log, callback)
-      }
+      // Notify event
+      debug('[%d] New log "%s" - %o', event.logIndex, event.event, event.args)
+      callback(null, event)
     }
   })
 }
---
+++ tests/playground/helpers/filterEthereumEvents.js
@@ -0,0 +1,46 @@
+const debug = require('debug')('DEBUG-dx-service:tests:helpers:watchEthereumEvents')
+const testSetup = require('../../helpers/testSetup')
+const ethereumEventHelper = require('../../../src/helpers/ethereumEventHelper')
+
+testSetup()
+  .then(run)
+  .catch(console.error)
+
+function run ({
+  dx,
+  botAccount
+}) {
+  ethereumEventHelper
+    .filter({
+      contract: dx,
+      filters: {
+        user: botAccount
+      },
+      fromBlock: 0, // 'latest'
+      toBlock: 'latest',
+      callback (error, event) {
+        if (error) {
+          console.error(error)
+        } else {
+        }
+      },
+      events: [
+        'NewSellOrder',
+        'NewBuyOrder'
+      ]
+    })
+    .then(events => {
+      debug('%d events:', events.length)
+      events.forEach(event => {
+        const { event: eventName, blockNumber } = event
+        const { sellToken, buyToken, auctionIndex, amount } = event.args
+
+        debug(`\t[blockNumber=${blockNumber}] ${eventName}:
+\t\t- Auction Index: ${auctionIndex}
+\t\t- Token Pair: ${sellToken}-${buyToken}
+\t\t- Amount: ${amount}\n`)
+
+      })
+    })
+    .catch(console.error)
+}
