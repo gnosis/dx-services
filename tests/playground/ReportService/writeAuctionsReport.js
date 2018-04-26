aaf4b8d20602af0988b29971428564241d33841c
Angel Rodriguez <anxolin@gmail.com>
2018-04-26 19:18:26 +0200

    Write report to disk

--- tests/playground/ReportService/writeAuctionsReport.js
+++ tests/playground/ReportService/writeAuctionsReport.js
@@ -1,5 +1,8 @@
 const testSetup = require('../../helpers/testSetup')
 const dateUtil = require('../../../src/helpers/dateUtil')
+const formatUtil = require('../../../src/helpers/formatUtil')
+const fs = require('fs')
+
 testSetup()
   .then(run)
   .catch(console.error)
@@ -8,8 +11,10 @@ function run ({
   reportService
 }) {
   const now = new Date()
-  const fromDate = dateUtil.toStartOf(now, 'day')
-  const toDate = dateUtil.toEndOf(now, 'day')
+  const fromDate = getDateFromEnv('FROM', dateUtil.toStartOf(now, 'day'))
+  const toDate = getDateFromEnv('TO', dateUtil.toEndOf(now, 'day'))
+  const fileName = getDateFromEnv('FILE', null)
+
   return reportService
     .getAuctionsReportFile({
       fromDate,
@@ -19,7 +24,22 @@ function run ({
       console.log('Generated file: "%s"', name)
       console.log('Mime type: "%s"', mimeType)
       console.log('Content:')
-      content.pipe(process.stdout)
+      if (fileName) {
+        console.log('Write result into: ./' + fileName)
+        const fileWriteStream = fs.createWriteStream('./' + fileName)
+        content.pipe(fileWriteStream)
+      } else {
+        content.pipe(process.stdout)
+      }
     })
     .catch(console.error)
+}
+
+function getDateFromEnv (envVarName, defaultValue) {
+  let date = process.env[envVarName]
+  if (date) {
+    return formatUtil.parseDateIso(date)
+  } else {
+    return defaultValue
+  }
 }
