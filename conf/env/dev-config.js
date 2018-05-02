79762817b31f1bbb46fe403014ca9f140553f33c
Angel Rodriguez <anxolin@gmail.com>
2018-05-02 17:24:59 +0200

    Update OMG and RDN addresses for Rinkeby

--- conf/env/dev-config.js
+++ conf/env/dev-config.js
@@ -3,8 +3,8 @@ const ENVIRONMENT = 'dev'
 // TODO: The env var MARKETS was disabled so in DEV they don't override it
 // we should uncoment it
 const MARKETS = [
-  { tokenA: 'WETH', tokenB: 'RDN' }
-  //,{ tokenA: 'WETH', tokenB: 'OMG' }
+  { tokenA: 'WETH', tokenB: 'RDN' },
+  { tokenA: 'WETH', tokenB: 'OMG' }
 ]
 
 module.exports = {
