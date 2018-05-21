3a0230f028c3cb3b843d48c7282fc8b272fe2730
Angel Rodriguez <anxolin@gmail.com>
2018-05-21 16:00:12 +0200

    Add closing price command

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -25,6 +25,7 @@ async function run (instances) {
   require('./cliCommands/auctionBalancesTokensCmd')(commandParams)
   require('./cliCommands/indexCmd')(commandParams)
   require('./cliCommands/approvedCmd')(commandParams)
+  require('./cliCommands/closingPriceCmd')(commandParams)
 
   // Trade commands
   require('./cliCommands/sendCmd')(commandParams)
