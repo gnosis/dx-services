9b4fe2a95223a55f1864d36ee3da629952f86772
Angel Rodriguez <anxolin@gmail.com>
2018-05-22 21:35:00 +0200

    Remove config for PRE in rinkeby

--- conf/network/rinkeby-config.js
+++ conf/network/rinkeby-config.js
@@ -2,23 +2,24 @@ const env = process.env.NODE_ENV
 let DX_CONTRACT_ADDRESS, RDN_TOKEN_ADDRESS, OMG_TOKEN_ADDRESS
 
 // In Rinkeby we use different instances of the contract for dev and staging
-if (env === 'pre') {
+if (env === 'pre' || env === 'pro') {
   // Rinkeby: staging
-  DX_CONTRACT_ADDRESS = '0x92f7da601234480433cc9e020710961bd1a2cc79'
+  //  We use set all addresses to null, because they should be provided
+  //  The DX contract address is loaded from the NPM package
+  DX_CONTRACT_ADDRESS = null
+  RDN_TOKEN_ADDRESS = null
+  OMG_TOKEN_ADDRESS = null
+} else if (env === 'dev') {
+  // Rinkeby: dev
+  //  We use a different DX contract than the one defined in the NPM package
+  DX_CONTRACT_ADDRESS = '0x6af10f3758912816c57e30cecd59c7695e1db2ee'
   RDN_TOKEN_ADDRESS = '0x7e2331beaec0ded82866f4a1388628322c8d5af0'
-  OMG_TOKEN_ADDRESS = '0xc57b5b272ccfd0f9e4aa8c321ec22180cbb56054' 
+  OMG_TOKEN_ADDRESS = '0xc57b5b272ccfd0f9e4aa8c321ec22180cbb56054'
 } else {
-  if (env === 'pro') {
-    // Rinkeby: pro
-    DX_CONTRACT_ADDRESS = null
-    RDN_TOKEN_ADDRESS = null
-    OMG_TOKEN_ADDRESS = null
-  } else {
-    // Rinkeby: dev, local, ...
-    DX_CONTRACT_ADDRESS = '0x6af10f3758912816c57e30cecd59c7695e1db2ee'
-    RDN_TOKEN_ADDRESS = '0x7e2331beaec0ded82866f4a1388628322c8d5af0'
-    OMG_TOKEN_ADDRESS = '0xc57b5b272ccfd0f9e4aa8c321ec22180cbb56054'
-  }
+  // Rinkeby: local
+  DX_CONTRACT_ADDRESS = null
+  RDN_TOKEN_ADDRESS = '0x7e2331beaec0ded82866f4a1388628322c8d5af0'
+  OMG_TOKEN_ADDRESS = '0xc57b5b272ccfd0f9e4aa8c321ec22180cbb56054'
 }
 
 module.exports = {
