2feb1e706c71003a0e3cdf61d6847ea3c7ca7c18
Angel Rodriguez <anxolin@gmail.com>
2018-07-11 18:54:50 +0200

    Update address for test rinkeby RDN and OMG

--- conf/network/rinkeby-config.js
+++ conf/network/rinkeby-config.js
@@ -15,14 +15,22 @@ if (env === 'pre' || env === 'pro') {
 } else if (env === 'dev') {
   // Rinkeby: dev
   //  We use a different DX contract than the one defined in the NPM package
-  DX_CONTRACT_ADDRESS = NETWORKS_DEV['DutchExchangeProxy']['4'].address
-  RDN_TOKEN_ADDRESS = '0x7e2331beaec0ded82866f4a1388628322c8d5af0'
-  OMG_TOKEN_ADDRESS = '0xc57b5b272ccfd0f9e4aa8c321ec22180cbb56054'
+  DX_CONTRACT_ADDRESS = NETWORKS_DEV['DutchExchangeProxy']['4'].address  
+  RDN_TOKEN_ADDRESS = '0x3615757011112560521536258c1e7325ae3b48ae'
+  OMG_TOKEN_ADDRESS = '0x00df91984582e6e96288307e9c2f20b38c8fece9'
+
+  // Old ones
+  // RDN_TOKEN_ADDRESS = '0x7e2331beaec0ded82866f4a1388628322c8d5af0'
+  // OMG_TOKEN_ADDRESS = '0xc57b5b272ccfd0f9e4aa8c321ec22180cbb56054'
 } else {
   // Rinkeby: local
   DX_CONTRACT_ADDRESS = null
-  RDN_TOKEN_ADDRESS = '0x7e2331beaec0ded82866f4a1388628322c8d5af0'
-  OMG_TOKEN_ADDRESS = '0xc57b5b272ccfd0f9e4aa8c321ec22180cbb56054'
+  RDN_TOKEN_ADDRESS = '0x3615757011112560521536258c1e7325ae3b48ae'
+  OMG_TOKEN_ADDRESS = '0x00df91984582e6e96288307e9c2f20b38c8fece9'
+
+  // Old ones
+  // RDN_TOKEN_ADDRESS = '0x7e2331beaec0ded82866f4a1388628322c8d5af0'
+  // OMG_TOKEN_ADDRESS = '0xc57b5b272ccfd0f9e4aa8c321ec22180cbb56054'
 }
 
 const URL_GAS_PRICE_FEED_GAS_STATION = null
