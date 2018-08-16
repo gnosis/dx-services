4715dafe988d5c5c4aaa5547af668edd21037901
Angel Rodriguez <anxolin@gmail.com>
2018-08-16 18:33:45 +0200

    Do not break if executed out of a git repo

--- src/helpers/getGitInfo.js
+++ src/helpers/getGitInfo.js
@@ -1,10 +1,20 @@
 var git = require('git-rev-sync')
 
 module.exports = function () {
-  return {
-    short: git.short(),
-    long: git.long(),
-    branch: git.branch(),
-    tag: git.tag()
+  try {
+    return {
+      short: git.short(),
+      long: git.long(),
+      branch: git.branch(),
+      tag: git.tag()
+    }
+  } catch (error) {
+    const msg = 'Not a git repo'
+    return {
+      short: msg,
+      long: msg,
+      branch: msg,
+      tag: msg
+    }
   }
 }
