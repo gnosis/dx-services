b4469638b62e422aeab56da36d2bf6e491c3d65f
Angel Rodriguez <anxolin@gmail.com>
2018-08-29 21:00:06 +0200

    Fix balances command

--- src/cli/cliCommands/balancesCmd.js
+++ src/cli/cliCommands/balancesCmd.js
@@ -18,7 +18,8 @@ function registerCommand ({ cli, instances, logger }) {
       const {
         botAccount,
         dxInfoService,
-        contracts
+        contracts,
+        config
       } = instances
 
       if (!account) {
@@ -30,55 +31,27 @@ function registerCommand ({ cli, instances, logger }) {
       logger.info('\tACCOUNT: %s', account)
       logger.info('\tBALANCE: %d ETH', formatUtil.formatFromWei(balanceETH))
 
-      const [ tokenList, magnoliaToken ] = await Promise.all([
-        dxInfoService.getTokenList(),
-        dxInfoService.getMagnoliaToken()
-      ])
+      // const [ tokenList, magnoliaToken ] = await Promise.all([
+      //   dxInfoService.getTokenList(),
+      //   dxInfoService.getMagnoliaToken()
+      // ])
+      // const tokens = tokenList.data
+      // tokens.push(magnoliaToken)
+      const tokens = Object.keys(config.ERC20_TOKEN_ADDRESSES).map(symbol => {
+        return {
+          symbol,
+          tokenAddress: config.ERC20_TOKEN_ADDRESSES[symbol]
+        }
+      })
 
-      const tokens = tokenList.data
-      tokens.push(magnoliaToken)
+      const balancePromises = tokens.map(token => _getBalance({
+        account,
+        token,
+        dxInfoService,
+        dxAddress: contracts.dx.address
+      }))
 
-      const balances = await Promise.all(
-        tokens.map(async ({ address: tokenAddress, symbol }) => {
-          return Promise
-            .all([
-              // get token balance
-              dxInfoService.getAccountBalanceForTokenNotDeposited({ tokenAddress, account }),
-              dxInfoService.getTokenAllowance({
-                tokenAddress,
-                owner: account,
-                spender: contracts.dx.address
-              }),
-              dxInfoService.getTokenTotalSupply({ tokenAddress }),
-              // get token balance in DX
-              dxInfoService.getAccountBalanceForToken({ token: symbol, address: account })
-            ])
-            .then(async ([ amount, allowance, totalSupply, amountInDx ]) => {
-              const priceUsdInDx = await dxInfoService
-                .getPriceInUSD({
-                  token: symbol, amount: amountInDx
-                })
-                .then(price => price.toFixed(2))
-                .catch(() => null)
-
-              const priceUsd = await dxInfoService
-                .getPriceInUSD({ token: symbol, amount })
-                .then(price => price.toFixed(2))
-                .catch(() => null)
-
-              return {
-                tokenAddress,
-                token: symbol,
-                amount,
-                allowance,
-                totalSupply,
-                amountInDx,
-                priceUsdInDx,
-                priceUsd
-              }
-            })
-        }))
-
+      const balances = await Promise.all(balancePromises)
       balances.forEach(balance => {
         logger.info('\n\tBalances %s (%s):', balance.token, balance.tokenAddress)
         logger.info(
@@ -100,6 +73,109 @@ function registerCommand ({ cli, instances, logger }) {
       })
       logger.info('\n**************************************\n\n')
     })
+}
+
+async function _getBalance ({
+  token,
+  dxInfoService,
+  account,
+  dxAddress
+}) {
+  const { tokenAddress, symbol } = token
+
+  const {
+    amount,
+    allowance,
+    totalSupply,
+    amountInDx
+  } = await _getBasicBalances({
+    tokenAddress,
+    symbol,
+    account,
+    dxInfoService,
+    dxAddress
+  })
+
+  const { priceUsdInDx, priceUsd } = await _getUsdEstimation({
+    amount,
+    amountInDx,
+    dxInfoService
+  })
+
+  return {
+    tokenAddress,
+    token: symbol,
+    amount,
+    allowance,
+    totalSupply,
+    amountInDx,
+    priceUsdInDx,
+    priceUsd
+  }
+}
+
+async function _getBasicBalances ({
+  tokenAddress,
+  symbol,
+  account,
+  dxInfoService,
+  dxAddress
+}) {
+  // TODO: Create a service function that return the balance details so we don't
+  // have to do so many service calls
+  const [
+    amount,
+    allowance,
+    totalSupply,
+    amountInDx
+  ] = await Promise.all([
+    // get token balance
+    dxInfoService.getAccountBalanceForTokenNotDeposited({ tokenAddress, account }),
+    dxInfoService.getTokenAllowance({
+      tokenAddress,
+      owner: account,
+      spender: dxAddress
+    }),
+    dxInfoService.getTokenTotalSupply({ tokenAddress }),
+    // get token balance in DX
+    dxInfoService.getAccountBalanceForToken({ token: symbol, address: account })
+  ])
+
+  return {
+    amount,
+    allowance,
+    totalSupply,
+    amountInDx
+  }
+}
+
+async function _getUsdEstimation ({
+  symbol,
+  amountInDx,
+  amount,
+  dxInfoService
+}) {
+  const priceUsdInDxPromise = dxInfoService
+    .getPriceInUSD({
+      token: symbol, amount: amountInDx
+    })
+    .then(price => price.toFixed(2))
+    .catch(() => null)
+
+  const priceUsdPromise = dxInfoService
+    .getPriceInUSD({ token: symbol, amount })
+    .then(price => price.toFixed(2))
+    .catch(() => null)
+
+  const [ priceUsdInDx, priceUsd ] = await Promise.all([
+    priceUsdInDxPromise,
+    priceUsdPromise
+  ])
+
+  return {
+    priceUsdInDx,
+    priceUsd
+  }
 }
 
 module.exports = registerCommand
