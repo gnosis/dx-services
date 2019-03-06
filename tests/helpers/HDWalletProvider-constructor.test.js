cc7efe2367f61d870a50563dde81c8664674369f
Angel Rodriguez <anxolin@gmail.com>
2019-03-06 19:44:23 +0100

    Create testUtil with mock data

--- tests/helpers/HDWalletProvider-constructor.test.js
+++ tests/helpers/HDWalletProvider-constructor.test.js
@@ -1,29 +1,19 @@
 const HDWalletProvider = require('../../src/helpers/web3Providers/HDWalletProvider')
-
-const mnemonic = 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat'
-const privateKeys = [
-  '0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3',
-  '0xae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f',
-  '0x0dbbe8e4ae425a6d2687f1a7e3ba17bc98c673636790f1b8ad91193c05875ef1'
-]
-// const [pk1, pk2, pk3] = privateKeys
-const addresses = [
-  '0x627306090abab3a6e1400e9345bc60c78a8bef57',
-  '0xf17f52151ebef6c7334fad080c5704d77216b732',
-  '0xc5fdf4076b8f3a5357c5e395ab970b5b54098fef'
-]
-const [address1, address2, address3] = addresses
+const {
+  MNEMONIC,
+  PRIVATE_KEYS,
+  ACCOUNTS,
+  RPC_URL
+} = require('../testUtil')
+const [ACCOUNT_1, ACCOUNT_2, ACCOUNT_3] = ACCOUNTS
 
 const WALLET_DEFAULTS = {
-  url: 'http://127.0.0.1:8545',
+  url: RPC_URL,
   addressIndex: 0,
   numAddresses: 3,
   shareNonce: true,
   blockForNonceCalculation: 'pending'
 }
-
-// beforeAll(() => {
-// })
 
 describe('Constructor validations', () => {
   test('It throws an error if no mnemonic or private key was provided', () => {
@@ -34,20 +24,19 @@ describe('Constructor validations', () => {
 })
 
 describe('Mnemonic account setup', () => {
-  // Applies only to tests in this describe block
   test('It generate 1 account', () => {
     // GIVEN: A mnemonic and numAddresses=1
     const config = {
       ...WALLET_DEFAULTS,
       numAddresses: 1,
-      mnemonic
+      mnemonic: MNEMONIC
     }
 
     // WHEN: Creating the wallet
     const wallet = new HDWalletProvider(config)
 
     // THEN: It returns the right address
-    expect(wallet.getAddresses()).toEqual([address1])
+    expect(wallet.getAddresses()).toEqual([ACCOUNT_1])
   })
 
   test('It generate 3 accounts', () => {
@@ -55,17 +44,17 @@ describe('Mnemonic account setup', () => {
     const config = {
       ...WALLET_DEFAULTS,
       numAddresses: 3,
-      mnemonic
+      mnemonic: MNEMONIC
     }
 
     // WHEN: Creating the wallet
     const wallet = new HDWalletProvider(config)
 
     // THEN: The addresses are the expected ones
-    expect(wallet.getAddresses()).toEqual(addresses)
-    expect(wallet.getAddress(0)).toEqual(address1)
-    expect(wallet.getAddress(1)).toEqual(address2)
-    expect(wallet.getAddress(2)).toEqual(address3)
+    expect(wallet.getAddresses()).toEqual(ACCOUNTS)
+    expect(wallet.getAddress(0)).toEqual(ACCOUNT_1)
+    expect(wallet.getAddress(1)).toEqual(ACCOUNT_2)
+    expect(wallet.getAddress(2)).toEqual(ACCOUNT_3)
   })
 })
 
@@ -73,7 +62,7 @@ describe('Private key account setup', () => {
   // Applies only to tests in this describe block
   test('It generate 1 account', () => {
     // GIVEN: A private key
-    const pk3 = privateKeys[2]
+    const pk3 = PRIVATE_KEYS[2]
     const config = {
       ...WALLET_DEFAULTS,
       numAddresses: 1,
@@ -84,7 +73,7 @@ describe('Private key account setup', () => {
     const wallet = new HDWalletProvider(config)
 
     // THEN: Returns the expected address
-    expect(wallet.getAddresses()).toEqual([address3])
+    expect(wallet.getAddresses()).toEqual([ACCOUNT_3])
   })
 
   test('It generate 3 accounts', () => {
@@ -92,16 +81,16 @@ describe('Private key account setup', () => {
     const config = {
       ...WALLET_DEFAULTS,
       numAddresses: 3,
-      privateKeys
+      privateKeys: PRIVATE_KEYS
     }
 
     // WHEN: Creating the wallet
     const wallet = new HDWalletProvider(config)
 
     // THEN: Returns the expected addresses
-    expect(wallet.getAddresses()).toEqual(addresses)
-    expect(wallet.getAddress(0)).toEqual(address1)
-    expect(wallet.getAddress(1)).toEqual(address2)
-    expect(wallet.getAddress(2)).toEqual(address3)
+    expect(wallet.getAddresses()).toEqual(ACCOUNTS)
+    expect(wallet.getAddress(0)).toEqual(ACCOUNT_1)
+    expect(wallet.getAddress(1)).toEqual(ACCOUNT_2)
+    expect(wallet.getAddress(2)).toEqual(ACCOUNT_3)
   })
 })
